02255babb8bdca8a6cdc732210e41d4e3d8a8bc9
David Sarkisian <dsarkisian@plansource.com>
2019-03-21 20:49:35 -0600

    Updated List and App.js files

---
+++ src/list/List.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Table, TableCell, Button, Checkbox} from 'semantic-ui-react';
+
+const List = ({ groceryItems, complete}) => (
+  <Table celled>
+    <Table.Header>
+      <Table.Row>
+        <Table.HeaderCell>Number</Table.HeaderCell>
+        <Table.HeaderCell>Name</Table.HeaderCell>
+        <Table.HeaderCell>Completion</Table.HeaderCell>
+      </Table.Row>
+    </Table.Header>
+
+    <Table.Body>
+      {
+        groceryItems.map(item => (
+          <Table.Row key={item.id}>
+            <TableCell>{item.id}</TableCell>
+            <TableCell style={ item.complete ? { ...styles.item, ...styles.complete } : styles.item }>{item.name}</TableCell>
+            <Table.Cell>
+              <Checkbox color='blue'  onClick={() => complete(item.id)} label={<label>Complete</label>} />
+            </Table.Cell>
+          </Table.Row>
+        ))
+      }
+    </Table.Body>
+  </Table>
+
+)
+
+const styles = {
+  complete: { color: 'grey', textDecoration: 'line-through' },
+};
+export default List
